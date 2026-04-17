c01ed8a111209ed8684d2ef2f429b25af0e970ec
Juan Carlos Pablo <jcpablo10@gmail.com>
2026-04-17 12:04:40 -0600

    feature(control props): implement Toggle and ParentComponent with control props pattern

--- src/components/ControlProps/ControlProps.tsx
+++ src/components/ControlProps/ControlProps.tsx
@@ -1,6 +1,37 @@
+// A parent can control the behavior of the child component
+
+// In this example the parent controls the behavior of the child. the compunication uses control prosp to handle the child
+// the child is customizable to reuse the component
+
+import { useState } from "react";
+
+type ToggleProps = {
+  isToggled?: boolean
+  onToggle?: (value: boolean) => void 
+}
+
+export const Toggle: React.FC<ToggleProps> = ({isToggled = false, onToggle}) => {
+  const [internalToggle, setInternalToggle] = useState(isToggled)
+
+  const toggle = () => {
+    setInternalToggle(!internalToggle)
+
+    if (onToggle) onToggle(!internalToggle)
+  }
+
+  return (
+    <button onClick={toggle}>
+      { internalToggle ? "on 😜" : "Off 😵" }
+    </button>
+  );
+};
+
 export const ParentComponent = () => {
+  const [toggle, setToggle] = useState(false)
   return (
     <div>
+      <p>Toggle is {toggle ? "👍" : "☹️"}</p>
+      <Toggle isToggled={toggle} onToggle={setToggle} />
     </div>
   );
 };
