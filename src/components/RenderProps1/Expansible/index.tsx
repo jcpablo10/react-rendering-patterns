353727c18603e217309a2d016d2e1d798a17d19e
Juan Carlos Pablo <jcpablo10@gmail.com>
2026-04-17 11:25:10 -0600

    feature(expansible component): add Expansible component with render props pattern

---
+++ src/components/RenderProps1/Expansible/index.tsx
@@ -0,0 +1,8 @@
+/**
+ * Excersice 1: Render Props
+ * - Build a Expansible component using the Render Props pattern.
+ * - The Expansible component should receive a toggleControl and a render function as props.
+ * - The toggleControl is the element that will be used to toggle the visibility of the content.
+ * - The render function should receive a toggleModal function as an argument, which will be used to toggle the visibility of the content.
+ * - The content should be rendered inside a div with a height of 0px when the content is hidden and 100px when the content is visible. The transition should be smooth.
+ */
--- src/components/RenderProps1/Modal/index.ts
+++
@@ -1,7 +0,0 @@
-type ModalProps = {
-  render: () => JSX.Element
-}
-
-export const Modal: React.FC<ModalProps> = ({render}) => {
-  return render()
-}
