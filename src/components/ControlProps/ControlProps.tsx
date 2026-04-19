c7b7d228069d75e95a186eaefaa0d9c8384a176b
Juan Carlos Pablo <jcpablo10@gmail.com>
2026-04-18 22:13:38 -0600

    refactor(Toggle component): simplify onToggle invocation logic

--- src/components/ControlProps/ControlProps.tsx
+++ src/components/ControlProps/ControlProps.tsx
@@ -1,22 +1,27 @@
-// A parent can control the behavior of the child component
+// A parent can control the behavior of the child component throgh props. 
+// This parent helps to manage the comunication between parent and child components.
 
 // In this example the parent controls the behavior of the child. the compunication uses control prosp to handle the child
 // the child is customizable to reuse the component
 
 import { useState } from "react";
 
+// Using types, could be helpfull to use union tupes. 
 type ToggleProps = {
   isToggled?: boolean
   onToggle?: (value: boolean) => void 
 }
 
+// Having an internal state, makes mor flexible the logic when we reuse the component
+// Fix example, its possbile that the parent component doest really need to handle the state or behavior of the child component.
 export const Toggle: React.FC<ToggleProps> = ({isToggled = false, onToggle}) => {
   const [internalToggle, setInternalToggle] = useState(isToggled)
 
   const toggle = () => {
     setInternalToggle(!internalToggle)
-
-    if (onToggle) onToggle(!internalToggle)
+    
+    // This also works
+    onToggle && onToggle(!internalToggle)
   }
 
   return (
